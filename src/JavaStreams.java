a7aaf049fe9d1536963dcb842589ee6e4559f46d
KEN20960 <muchirinephat5@gmail.com>
2022-09-15 09:47:03 +0300

    Imperative

--- src/JavaStreams.java
+++ src/JavaStreams.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @ Author NMuchiri
  **/
@@ -6,7 +9,32 @@ public class JavaStreams {
 
     public static void main(String[] args) {
 
+        List<Person> people = getPeople();
+
+        // Iterating using the old imperative approach
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people){
+            if (person.getGender().equals(Gender.FEMALE)){
+                females.add(person);
+            }
+        }
+
+        females.forEach(System.out::println);
+
     }
 
 
+    private static List<Person> getPeople(){
+        return
+                List.of(
+                        new Person("James Bond", 23, Gender.MALE),
+                        new Person("Caterina Rostov", 24, Gender.FEMALE),
+                        new Person("Hellen Jepkosgei", 24, Gender.FEMALE),
+                        new Person("Ross White", 24, Gender.MALE),
+                        new Person("Vladmir Smith", 24, Gender.MALE),
+                        new Person("Jane Doe", 23, Gender.FEMALE)
+
+                );
+    }
 }
