8592f60231d90bd1f1b14ab65224bd90cbe2c548
KEN20960 <muchirinephat5@gmail.com>
2022-09-15 09:38:23 +0300

    Java 8 Streams

---
+++ src/Gender.java
@@ -0,0 +1,7 @@
+/**
+ * @ Author NMuchiri
+ **/
+public enum Gender {
+
+    MALE, FEMALE
+}
---
+++ src/JavaStreams.java
@@ -0,0 +1,12 @@
+/**
+ * @ Author NMuchiri
+ **/
+public class JavaStreams {
+    // In this class, we'll learn more about functional programming and JAVA 8 Streams
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
